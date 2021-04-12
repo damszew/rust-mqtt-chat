6a1a8ed7642bb7658bd42b14df54f10ac4a4c753
damszew <damian.szewczyk111@gmail.com>
2021-04-12 19:24:23 +0200

    Improve testability of event handler - prepare for writting ut

--- src/events_handler/crossterm_events_handler.rs
+++ src/events_handler/crossterm_events_handler.rs
@@ -5,14 +5,17 @@ use tokio::sync::mpsc;
 
 use super::EventHandler;
 
-pub struct CrosstermEventsHandler {
+pub struct CrosstermEventsHandler<S>
+where
+    S: Stream,
+{
     sender: mpsc::Sender<()>,
-    event_stream: Fuse<EventStream>, // do not allow calls to stream after it finished
+    event_stream: S, // TODO: Try to make it `Fuse`
 }
 
-impl CrosstermEventsHandler {
+impl CrosstermEventsHandler<EventStream> {
     pub fn new(sender: mpsc::Sender<()>) -> Self {
-        let event_stream = EventStream::new().fuse();
+        let event_stream = EventStream::new();
 
         Self {
             sender,
@@ -22,7 +25,10 @@ impl CrosstermEventsHandler {
 }
 
 #[async_trait::async_trait]
-impl EventHandler for CrosstermEventsHandler {
+impl<S> EventHandler for CrosstermEventsHandler<S>
+where
+    S: Stream<Item = crossterm::Result<Event>> + Unpin + Send,
+{
     async fn dispatch_events(&mut self) -> anyhow::Result<()> {
         while let Some(Ok(event)) = self.event_stream.next().await {
             match event {
@@ -38,4 +44,29 @@ impl EventHandler for CrosstermEventsHandler {
     }
 }
 
-// TODO: write FakeEventStream for test
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio_stream::StreamExt;
+
+    #[tokio::test]
+    async fn test_name() {
+        let event = Event::Key(KeyEvent::new(KeyCode::Esc, KeyModifiers::NONE));
+        let (sender, mut receiver) = mpsc::channel(1);
+
+        let stream = tokio_stream::iter(vec![Ok(event)]);
+
+        let mut tested_event_handler = CrosstermEventsHandler {
+            event_stream: stream,
+            sender,
+        };
+
+        tested_event_handler.dispatch_events().await.unwrap();
+
+        let event = tokio::time::timeout(std::time::Duration::from_millis(100), async move {
+            receiver.recv().await.unwrap();
+        })
+        .await
+        .unwrap();
+    }
+}
