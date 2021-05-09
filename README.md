3819e18ad95ec95ba7a546bd3bd6323d016aa336
damszew <damian.szewczyk111@gmail.com>
2021-05-09 08:06:11 +0200

    Handle cmdl arguments

--- README.md
+++ README.md
@@ -13,8 +13,27 @@ Clone this repo and run `cargo build` to compile.
 
 ## Usage
 
+```
+rust mqtt chat 0.1.0
+Payload encrypted chat over mqtt (written in rust)
+
+USAGE:
+    actor-model-chat --password <password> --room <room> --server <server> --user <user>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -p, --password <password>    Rooms password [env: PASSWORD=]
+    -r, --room <room>            Name of chat room to connect to [env: ROOM=]
+    -s, --server <server>        Url to mqtt server [env: SERVER=]
+    -u, --user <user>            User name [env: USER=damian]
+```
+
+For example:
 ```bash
-cargo run -- --server tcp://localhost:1883 --room kitchen --user chef --password knife
+cargo run -- --server tcp://localhost:1883 --room kitchen --password pizza --user chef
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,41 @@ use actor_model_chat::{
     app::App, events_reader::terminal::CrosstermEventsHandler, network::setup_network,
     renderer::terminal_renderer::TerminalRenderer,
 };
+
 use anyhow::Result;
+use structopt::StructOpt;
+
+const TOPIC_PREFIX: &str = "df9ff5c8-c030-4e4a-8bae-a415565febd7";
+
+#[derive(StructOpt)]
+#[structopt(
+    name = "rust mqtt chat",
+    about = "Payload encrypted chat over mqtt (written in rust)"
+)]
+struct Opt {
+    /// Url to mqtt server
+    #[structopt(short, long, env)]
+    server: String,
+
+    /// Name of chat room to connect to
+    #[structopt(short, long, env)]
+    room: String,
+
+    /// Rooms password
+    #[structopt(short, long, env)]
+    password: String,
+
+    /// User name
+    #[structopt(short, long, env)]
+    user: String,
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
+    let opt = Opt::from_args();
+
     let (events_publisher, network_events) =
-        setup_network("tcp://localhost:1883/", "topic", "chat").await?;
+        setup_network(opt.server, TOPIC_PREFIX, opt.room).await?;
     let terminal_events = CrosstermEventsHandler::new();
     let renderer = TerminalRenderer::new(std::io::stdout())?;
 
