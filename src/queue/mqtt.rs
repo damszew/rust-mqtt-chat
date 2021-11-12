5cb0321f371936b62bb139c66d56431d7e06ffc9
damszew <damian.szewczyk111@gmail.com>
2021-11-12 17:07:39 +0000

    Add ctor for MqttQueue

--- src/queue/mqtt.rs
+++ src/queue/mqtt.rs
@@ -7,6 +7,23 @@ pub struct MqttQueue {
     receiver: futures::channel::mpsc::Receiver<Option<paho_mqtt::Message>>,
 }
 
+impl MqttQueue {
+    pub async fn new(url: String) -> Result<Self, Error> {
+        let opts = paho_mqtt::CreateOptionsBuilder::new()
+            .server_uri(url)
+            .finalize();
+        let mut client = paho_mqtt::AsyncClient::new(opts)?;
+        let receiver = client.get_stream(1);
+
+        let conn_opts = paho_mqtt::ConnectOptionsBuilder::new()
+            .keep_alive_interval(std::time::Duration::from_secs(30))
+            .finalize();
+        client.connect(conn_opts).await?;
+
+        Ok(Self { client, receiver })
+    }
+}
+
 #[async_trait::async_trait]
 impl Queue for MqttQueue {
     async fn publish(&self, topic: String, message: Message) -> Result<(), Error> {
