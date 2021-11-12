c5fb97116fa52873616949e9efeda94fd2b1d0bd
damszew <damian.szewczyk111@gmail.com>
2021-11-12 17:09:51 +0000

    Create combined object for encryption and decryption

--- src/crypt/magic_crypt.rs
+++ src/crypt/magic_crypt.rs
@@ -44,3 +44,34 @@ impl Decrypt for MagicDecrypt {
             .map_err(|e| anyhow::anyhow!(e))
     }
 }
+
+pub struct MagicCrypt {
+    crypt: MagicCrypt256,
+}
+
+impl MagicCrypt {
+    pub fn new(password: &impl AsRef<str>) -> Self {
+        let crypt = new_magic_crypt!(password, 256);
+        Self { crypt }
+    }
+}
+
+impl Encrypt for MagicCrypt {
+    fn encrypt<T>(&self, data: T) -> Vec<u8>
+    where
+        T: AsRef<[u8]>,
+    {
+        self.crypt.encrypt_to_bytes(&data)
+    }
+}
+
+impl Decrypt for MagicCrypt {
+    fn decrypt<T>(&self, data: T) -> Result<Vec<u8>>
+    where
+        T: AsRef<[u8]>,
+    {
+        self.crypt
+            .decrypt_bytes_to_bytes(&data)
+            .map_err(|e| anyhow::anyhow!(e))
+    }
+}
