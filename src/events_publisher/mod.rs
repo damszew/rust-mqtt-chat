e4e491de314de8cb864d48472630dca473bea091
damszew <damian.szewczyk111@gmail.com>
2021-11-17 06:44:26 +0000

    Remove old stuff

--- src/events_publisher/mod.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Result;
-
-pub mod mqtt;
-
-pub trait EventsPublisher {
-    type Message;
-
-    fn publish(&self, message: Self::Message) -> Result<()>;
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,5 @@
-pub mod app;
-pub mod crypto;
-pub mod events_publisher;
-pub mod events_reader;
-pub mod network;
-pub mod renderer;
-
 pub mod chat_room;
+pub mod crypto;
 pub mod queue;
 pub mod terminal_driver;
 pub mod ui;
-
-#[derive(Clone, Debug, PartialEq)]
-pub enum TerminalEvent {
-    Quit,
-    Character(char),
-    Accept,
-    Remove,
-    RemoveLast,
-    CursorLeft,
-    CursorRight,
-    CursorStart,
-    CursorEnd,
-    ScrollUp,
-    ScrollDown,
-}
